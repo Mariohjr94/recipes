c1a59779767d20df3bd0560c59a1405a69c66293
Mariohjr94 <mariohjr94@gmail.com>
2024-12-04 00:37:51 -0500

    fixing bugs on deployed app

--- client/src/features/RecipeDetails.jsx
+++ client/src/features/RecipeDetails.jsx
@@ -119,28 +119,7 @@ useEffect(() => {
   if (error) return <p>{error}</p>;
 
   return (
-    <div className="container">
- <div className="recipe-details container mt-5">
-  {!editMode ? (
-    <>
-      {/* Recipe Title */}
-      <h1 className="text-center mb-4">{recipe.name}</h1>
-
-      {/* Recipe Image */}
-      <div className="text-center mb-4">
-        <img
-          className="img-fluid rounded shadow"
-          style={{ maxWidth: "400px", cursor: "pointer" }}
-          src={recipe?.image}
-          alt={recipe?.name || "Recipe image"}
-          onClick={() => window.open(recipe.image, "_blank")}
-        />
-      </div>
-
-      {/* Recipe Details */}
-      <div className="row">
-        {/* Ingredients Section */}
-        <div cla<div className="container mt-5">
+ <div className="container mt-5">
   {!editMode ? (
     <div className="card shadow-lg">
       {/* Card Header */}
@@ -323,8 +302,6 @@ useEffect(() => {
           </div>
       )}
     </div>
-    </div>
-   
   );
 }
 
