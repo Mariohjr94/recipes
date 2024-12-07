129e828c04acd319828621690d8a77de551f1f67
Mariohjr94 <mariohjr94@gmail.com>
2024-12-07 01:37:43 -0500

    finishing app

--- client/src/features/RecipeDetails.jsx
+++ client/src/features/RecipeDetails.jsx
@@ -176,7 +176,7 @@ useEffect(() => {
           <div className="card-body">
             <div className="row">
               {/* image */}
-              <div className="col-md-6 d-flex align-items-center mt-5">
+              <div className="col-md-6 d-flex align-items-center mt-4">
                 <div className="text-center">
                   <img
                     className="img-fluid rounded"
