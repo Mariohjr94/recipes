846a4fb4d69b0edb28c3e1e8c001d9c876b5f120
Mariohjr94 <mariohjr94@gmail.com>
2024-12-25 17:25:08 -0500

    UI fixes

--- client/src/features/FreezerLog.jsx
+++ client/src/features/FreezerLog.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function FreezerLog() {
