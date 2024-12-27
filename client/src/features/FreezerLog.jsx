816920d6f1931e5efd5d0e1fc21ecdcd0ccf334b
Mariohjr94 <mariohjr94@gmail.com>
2024-12-27 03:57:15 -0500

    making freezer log available to public

--- client/src/features/FreezerLog.jsx
+++ client/src/features/FreezerLog.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function FreezerLog() {
   const [freezerItems, setFreezerItems] = useState([]);
