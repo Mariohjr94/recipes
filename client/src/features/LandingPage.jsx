f73ee0c5782612c433a15c27f475f1c212fac51a
Mariohjr94 <mariohjr94@gmail.com>
2024-12-27 00:44:08 -0500

    ui changes

--- client/src/features/LandingPage.jsx
+++ client/src/features/LandingPage.jsx
@@ -124,7 +124,7 @@ useEffect(() => {
                 <div className="card h-100">
                   <img src={recipe.image} className="card-img-top" alt={recipe.name} />
                     <div className="card-body">
-                      <h5 className="card-title text-center">{recipe.name}</h5>
+                      <p className="card-title text-center">{recipe.name}</p>
                     </div>
                 </div>
               </Link>
