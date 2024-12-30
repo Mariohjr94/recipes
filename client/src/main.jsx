28e233fcc56ba34e573f5789d8f0027682cfdc18
Mariohjr94 <mariohjr94@gmail.com>
2024-12-30 14:03:50 -0500

    fixing the alphabetical sorter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import store from './app/store.js';
 import App from './app/App'
 
