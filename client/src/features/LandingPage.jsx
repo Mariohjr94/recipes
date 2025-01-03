e0cf39e09a0fbd5fb90b8c446f37f2c786a94a40
Mariohjr94 <mariohjr94@gmail.com>
2025-01-03 10:37:43 -0500

    adding masonry layout style to my landing page

--- client/src/features/LandingPage.jsx
+++ client/src/features/LandingPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Masonry from "react-masonry-css";
+import Masonry from "masonry-layout";
 
 function LandingPage() {
   const [recipes, setRecipes] = useState([]);
